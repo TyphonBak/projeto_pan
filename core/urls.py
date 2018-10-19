10e6f622dc9da8dd32bbf89504e10cf07dc1189a
Cezar Filho <andrade.czr@gmail.com>
2018-10-19 09:09:16 -0300

    v1.1

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from core.views import *
 
 urlpatterns = [
-    path('', index)
+    path('', login),
+    path('index/', index),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+def login(request):
+    return render(request, "login.html")
+
 def index(request):
-    return render(request, "index.html")
+    
+    context = {
+        'title': 'Sabadin - Index',
+        'setor': 'Inicio'
+    }
+
+    return render(request, "index.html", context)
