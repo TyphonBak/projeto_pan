0b4dac0881415a8e691309d8c750a05fe02cc82a
Cezar Filho <andrade.czr@gmail.com>
2018-11-09 10:14:03 -0200

    paths adjust

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from core.views import *
 urlpatterns = [
     path('', login, name='login'),
     path('index/', index, name='index'),
-    path('step/new', step_new, name='step_new'),
-    path('process/new', process_new, name='process_new'),
+    path('step/', step_new, name='step_new'),
+    path('process/', process_new, name='process_new'),
     re_path(r'^process/(?P<process_id>\d+)$', process_config, name='process_config'),
     re_path(r'^process/(?P<process_name>\w+)/config$', process_config_new, name='process_config_new'),
     re_path(r'^process/(?P<process_name>\w+)/config/(?P<processstep_id>\d+)/$', process_config_step, name='process_config_step'),
